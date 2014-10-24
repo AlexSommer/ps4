e2b9699e3c77ceeaed61aa74b566e884ab4a0128
AlexSommer <ajs455@cornell.edu>
2014-10-23 21:09:43 -0400

    cleaned up a bit

--- interpreter/eval.ml
+++ interpreter/eval.ml
@@ -30,33 +30,6 @@ exception InvalidArguments
 
 exception Placeholder
 
-let rec print_datum input =
-  let string_of_cons car cdr =
-    let rec strings_of_cons cdr =
-      match cdr with
-      | Nil -> []
-      | Cons (car, cdr) -> (print_datum car) :: (strings_of_cons cdr)
-      | _ -> ["."; print_datum cdr;] in
-    let string_list = (print_datum car) :: (strings_of_cons cdr) in
-    "(" ^ (String.concat " " string_list) ^ ")"  in
-  match input with
-  | Atom (Boolean b) -> "Atom (Boolean " ^ (if b then "#t" else "#f") ^ ")"
-  | Atom (Integer n) -> "Atom (Integer " ^ (string_of_int n) ^ ")"
-  | Atom (Identifier id) -> "Atom (Identifier " ^ (Identifier.string_of_identifier id) ^")"
-  | Nil -> "Nil"
-  | Cons (car, cdr) -> "Cons (" ^ string_of_cons car cdr ^ ")"
-
-(* let rec print_datum input =
-  match input with
-  | Atom (Boolean b) -> "Atom (Boolean " ^ (if b then "#t" else "#f") ^ ")"
-  | Atom (Integer n) -> "Atom (Integer " ^ (string_of_int n) ^ ")"
-  | Atom (Identifier id) -> "Atom (Identifier " ^ (Identifier.string_of_identifier id) ^")"
-  | Nil -> "Nil"
-  | Cons (car, cdr) -> "Cons (" ^ (print_datum car) ^ ", " ^ (print_datum cdr) ^ ")" *)
-
-
-
-
 (* Parses a datum into an expression. *)
 let rec read_expression (input : datum) : expression =
   match input with
@@ -111,7 +84,8 @@ and build_vars_args (acc) (bindLst:datum) =
   | Cons (Cons (Atom (Identifier name), Cons(exp, Nil)), tail) when
       (Identifier.is_valid_variable name) ->
        build_vars_args 
-       (acc@[((Identifier.variable_of_identifier name),(read_expression exp))]) tail
+       (acc@[((Identifier.variable_of_identifier name),
+          (read_expression exp))]) tail
   | _ -> failwith "Invalid Let Expression"
 
 and parse (dat:datum) : expression list =
@@ -133,8 +107,6 @@ and parse_vars (dat:datum) : variable list =
   | _ -> failwith "parsing variable error"
 
 
-
-
 (* Parses a datum into a toplevel input. *)
 let read_toplevel (input : datum) : toplevel =
 (*   print_string (print_datum input); *)
@@ -146,45 +118,7 @@ let read_toplevel (input : datum) : toplevel =
         (read_expression tl))
   | _ -> ToplevelExpression (read_expression input)
 
-
-
-  (* equal? and cons parsing *)
- (*  | Cons ((Atom (Identifier id)), Cons (e1, Cons (e2, Nil))) 
-    when (((Identifier.string_of_identifier id) = "equal?") ||
-        ((Identifier.string_of_identifier id) = "cons")) ->
-      ExprProcCall (ExprVariable (Identifier.variable_of_identifier id), 
-        [read_expression e1;read_expression e2])
-  | Cons ((Atom (Identifier id)), Cons (e1, Nil))
-      when ((Identifier.string_of_identifier id) = "eval") ->
-      ExprProcCall (ExprVariable (Identifier.variable_of_identifier id),
-        [read_expression e1]) *)
-
-
-  (* car/cdr parsing *)
-(*   | Cons (Atom (Identifier id), Cons (all, Nil)) ->
-      ExprProcCall (ExprVariable (Identifier.variable_of_identifier id), 
-        [read_expression all]) *)
-
-(*   | Cons (Cons (Atom (Identifier id), Cons (vars, Cons (body,Nil))), tail) 
-      when ((Identifier.string_of_identifier id) = "lambda") ->
-      (print_string "3");
-      ExprProcCall (ExprLambda ((parse_vars vars),(parse body)),(parse tail)) *)
-    (* generic proc call match *)
-(*   | Cons (Atom (Identifier id), e1) ->
-      (print_string "2");
-      ExprProcCall (ExprVariable (Identifier.variable_of_identifier id), 
-        (parse e1)) *)
-
-
-
-
-
-
-
-
-
 (* -------------------EVALUATION HELPER FUNCTIONS ------------------- *)
-
 
 let equal_helper (lst:value list) (env:environment) : value = 
   match lst with
@@ -247,12 +181,10 @@ let rec contains_dup (lst:'a list) : bool =
         contains_dup tl
 
 let rec process_variable (var:Identifier.variable) (env:environment) : value =
-  (print_string (Identifier.string_of_variable var));
   if (Environment.is_bound env var) then
     !(Environment.get_binding env var)
   else 
     raise VariableNotDefined
-
 
 
 (* This function returns an initial environment with any built-in
@@ -284,42 +216,11 @@ let rec initial_environment () : environment =
   let env8 = Environment.add_binding env7
     (eighth, ref (ValProcedure (ProcBuiltin multiply_helper))) in env8
 
-
 and process_if e1 e2 e3 env =
   if ((eval e1 env) = (ValDatum (Atom (Boolean false)))) then
     eval e3 env
   else 
     eval e2 env
-
-(* and process_proc_call var lst env =
-  (print_string "in process proc call");
-  if (("equal?" = (Identifier.string_of_variable var)) || 
-      ("cons" = (Identifier.string_of_variable var))) then       
-    match (lst,!(Environment.get_binding env var)) with
-    | (hd1::hd2::[],ValProcedure (ProcBuiltin f)) -> 
-      begin
-        let first = (eval hd1 env) in
-        let second = (eval hd2 env) in f [first;second] env
-      end
-    | _ -> raise InvalidArguments
-  else if (("eval" = (Identifier.string_of_variable var)) || 
-    ("car" = (Identifier.string_of_variable var)) ||
-      ("cdr" = (Identifier.string_of_variable var))) then
-        match (lst,!(Environment.get_binding env var)) with
-        | (hd::[],ValProcedure (ProcBuiltin f)) -> 
-            let first = eval hd env in f [first] env
-        | _ -> raise InvalidArguments
-  else if (("+" = (Identifier.string_of_variable var)) ||
-        ("*" = (Identifier.string_of_variable var))) then
-    let changed = List.map (fun ele -> eval ele env) lst in
-    match !(Environment.get_binding env var) with
-    | ValProcedure (ProcBuiltin f) -> f changed env
-    | _ -> raise InvalidArguments
-  else if (Environment.is_bound env var) then
-    let closure = process_variable var env in
-    process_proc_lambda closure lst env
-  else
-    raise InvalidArguments *)
 
 and eval_body (body:expression list) (env:environment) : value =
   match body with
@@ -346,12 +247,6 @@ and process_proc_lambda (clos:value) (args:expression list) (env:environment) =
         f changed env
   | _ -> failwith "Invalid Procedure Call"
 
-(* 
-*******************************************************
-MAKE SURE YOU CHECK THAT THIS IS NOT A MUTABLE ENVIRONMENT!!!!!!!!!!!
--------------KD29341982374902471974190274187234871209487
-DSKFJA;DLKFJAL;DFJA;LSDJFAL;SDKJF;ALKSDJF;LAKSDJF
- *)
 and process_lambda (varlst:variable list) (exprlst:expression list)
     (env:environment) : value =
   if (contains_dup varlst) then
@@ -373,7 +268,6 @@ and process_def (var:variable) (expr:expression) (env:environment)
 
 and process_let (bindList:let_binding list) (expList:expression list)
     (env:environment) : value =
-  (print_string "here");
   let (vars,args) = List.split bindList in
   if (contains_dup vars) then
     failwith "Invalid Let Structure"
@@ -392,14 +286,6 @@ and process_let_star (bindList:let_binding list) (expList:expression list)
   | (name,value)::tl ->
       let newEnv = Environment.add_binding env (name,(ref (eval value env))) in
       process_let_star tl expList newEnv
-
-(* and process_let_rec (bindList:let_binding list) (expList:expression list)
-    (env:environment) : value =
-  let changed = List.map (fun (var,value) -> (var,eval value env)) bindList in
-  let changed2 = List.map (fun (var,value) -> (var,ref value)) changed in
-  let envExtend = 
-    List.fold_left Environment.add_binding env changed2 in
-  process_let bindList expList envExtend *)
 
 and process_let_rec (bindList:let_binding list) (expList:expression list)
     (env:environment) : value =
@@ -448,7 +334,6 @@ let eval_toplevel (toplevel : toplevel) (env : environment) :
   | ToplevelExpression expression -> (eval expression env, env)
   | ToplevelDefinition (var, expr) -> process_def var expr env
      
-
 let rec string_of_value value =
   let rec string_of_datum datum =
     match datum with
