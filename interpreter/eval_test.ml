b5f9e8f1b96ab9c5c55a59179339d3a30c181b47
mfcripps <mfc84@cornell.edu>
2014-10-23 23:42:36 -0400

    eval_test explain

---
+++ interpreter/eval_test.ml
@@ -0,0 +1,5 @@
+In terms of testing problem 3, multiple TA's told us it was much more important to test
+extensively the functionality in the top level, and therefore we dedicated the time that
+we would have used to write unit tests to testing everything we could think of in the top
+level. So while we don't have unit tests, we tested extensively, and TA's told us that this 
+was OK. 
